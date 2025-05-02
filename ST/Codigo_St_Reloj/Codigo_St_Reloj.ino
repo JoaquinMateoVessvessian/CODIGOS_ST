2235b792b84bf84c10b2a3395726956209c01d0b
JoaquinMateoVessvessian <joaquinvessvessian21@gmail.com>
2025-05-02 12:16:14 -0300

    Code

--- ST/Codigo_St_Reloj/Codigo_St_Reloj.ino
+++ ST/Codigo_St_Reloj/Codigo_St_Reloj.ino
@@ -10,7 +10,7 @@
 
 #include <Adafruit_Sensor.h>
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
-void Maquina(float t, int Estado, int EstadoBoton1, int EstadoBoton2);
+void Maquina(float t, int EstadoBoton1, int EstadoBoton2);
 #define DHTPIN 23
 #define DHTTYPE DHT11
 #define P1 200
@@ -20,19 +20,24 @@ void Maquina(float t, int Estado, int EstadoBoton1, int EstadoBoton2);
 #define SUMAMIN 600
 #define BOTON1 35
 #define BOTON2 34
+int hora;
+int minute;
 int boton1;
 int boton2;
-int hora = 11;
-int minute = 39;
-ESP32Time rtc(3600);
+ESP32Time rtc(0);
 int estado;
+bool PASAJE = false;
 DHT dht(DHTPIN, DHTTYPE);
 void setup() {
   Serial.begin(9600);
-  rtc.setTime(30, minute, hora, 25, 4, 2025);
+  rtc.setTime(30, 38, 11, 25, 4, 2025);
   Serial.println(F("OLED test"));
+  pinMode(BOTON1, INPUT_PULLUP);
+  pinMode(BOTON2, INPUT_PULLUP);
   u8g2.begin();
   dht.begin();
+  hora = rtc.getHour();
+  minute = rtc.getMinute();
   estado = P1;
 }
 
@@ -40,67 +45,70 @@ void loop() {
   float t = dht.readTemperature();
   boton1 = digitalRead(BOTON1);
   boton2 = digitalRead(BOTON2);
-  Maquina(t, estado, boton1, boton2);
-  delay(2000);
+  Maquina(t, boton1, boton2);
+  delay(100);
 }
-void Maquina(float t, int Estado, int EstadoBoton1, int EstadoBoton2) {
-  bool PASAJE;
+void Maquina(float t, int EstadoBoton1, int EstadoBoton2) {
   char stringt[6];
-  char stringtiempo[20];
+  char stringtiempo[10];
   sprintf(stringt, "%.2f", t);
-  sprintf(stringtiempo, "%d", "%a, %b %d %y %H:%M:%S");
-  switch (Estado) {
+  switch (estado) {
     case P1:
+      sprintf(stringtiempo, "%02d:%02d  ", hora, minute);
       u8g2.clearBuffer();
-      char stringt[6];
-      char stringtiempo[6];
       u8g2.setFont(u8g2_font_ncenB08_tr);
-      u8g2.drawStr(15, 15, stringt);
-      u8g2.drawStr(30, 15, stringtiempo);
+      u8g2.drawStr(15, 15, "Temp:");
+      u8g2.drawStr(60, 15, stringt);
+      u8g2.drawStr(15, 30, "Hora:");
+      u8g2.drawStr(60, 30, stringtiempo);
       u8g2.sendBuffer();
       PASAJE = true;
-      if (EstadoBoton1 == 1 && EstadoBoton2 == 1) {
-        Estado = ESPERA;
+      if (EstadoBoton1 == LOW && EstadoBoton2 == LOW) {
+        estado = ESPERA;
       }
       break;
     case ESPERA:
-      if (EstadoBoton1 == 0 && EstadoBoton2 == 0) {
+      if (EstadoBoton1 == HIGH && EstadoBoton2 == HIGH) {
         if (PASAJE == false) {
-          Estado = P1;
+          estado = P1;
         }
         if (PASAJE == true) {
-          Estado = P2;
+          estado = P2;
         }
       }
       break;
     case P2:
+      sprintf(stringtiempo, "%02d:%02d  ", hora, minute);
       u8g2.clearBuffer();
-      if (EstadoBoton1 == 1) {
-        Estado = SUMAHORA;
+      u8g2.drawStr(15, 30, "Hora:");
+      u8g2.drawStr(60, 30, stringtiempo);
+      if (EstadoBoton1 == LOW) {
+        estado = SUMAHORA;
       }
-      if (EstadoBoton2 == 1) {
-        Estado = SUMAMIN;
+      if (EstadoBoton2 == LOW) {
+        estado = SUMAMIN;
       }
-      u8g2.drawStr(15, 15, stringtiempo);
       u8g2.sendBuffer();
       PASAJE = false;
-      if (EstadoBoton1 == 1 && EstadoBoton2 == 1) {
-        Estado = ESPERA;
+      if (EstadoBoton1 == LOW && EstadoBoton2 == LOW) {
+        estado = ESPERA;
       }
       break;
     case SUMAHORA:
       hora = hora + 1;
-      if (hora > 60) {
+      if (hora >= 24) {
         hora = 0;
       }
-      Estado = P2;
+      estado = P2;
       break;
+
     case SUMAMIN:
       minute = minute + 1;
-      if (minute > 60) {
+      if (minute >= 60) {
         minute = 0;
+        hora = hora + 1;
       }
-      Estado = P2;
+      estado = P2;
       break;
   }
 }
