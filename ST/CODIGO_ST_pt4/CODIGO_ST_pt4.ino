ab2e22817011462888426dfec0b33dd5c49ec204
JoaquinMateoVessvessian <joaquinvessvessian21@gmail.com>
2025-05-06 10:08:36 -0300

    Codes

--- ST/CODIGO_ST_pt4/CODIGO_ST_pt4.ino
+++ ST/CODIGO_ST_pt4/CODIGO_ST_pt4.ino
@@ -22,6 +22,8 @@ int Umbral = 28;
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
 DHT dht(DHTPIN, DHTTYPE);
 void maquina_de_estados(int Estado, float temperatura, int EstadoBoton1, int EstadoBoton2);
+int boton1;
+int boton2;
 void setup() {
   Serial.begin(9600);
   Serial.println(F("OLED test"));
@@ -30,25 +32,26 @@ void setup() {
   Estado = P1;
   pinMode(BOTON1, INPUT_PULLUP);
   pinMode(BOTON2, INPUT_PULLUP);
+  pinMode(25, OUTPUT);
 }
 
 void loop() {
-  int boton1 = digitalRead(BOTON1);
-  int boton2 = digitalRead(BOTON2);
+  boton1 = digitalRead(BOTON1);
+  boton2 = digitalRead(BOTON2);
   float temperatura = dht.readTemperature();
   maquina_de_estados(Estado, temperatura, boton1, boton2);
   delay(100);
 }
 
 void maquina_de_estados(int Estado, float temperatura, int EstadoBoton1, int EstadoBoton2) {
-  char stemperatura[5];
+  char stemperatura[6];
   char sUmbral[5];
-  sprintf(stemperatura, "%.2f", temperatura);
-  sprintf(sUmbral, "%d", Umbral);
   switch (Estado) {
     case P1:
       {
         u8g2.clearBuffer();
+        sprintf(stemperatura, "%.2f", temperatura);
+        sprintf(sUmbral, "%d", Umbral);
         u8g2.setFont(u8g2_font_ncenB14_tr);
         u8g2.drawStr(15, 15, stemperatura);
         u8g2.drawStr(15, 30, sUmbral);
@@ -61,6 +64,7 @@ void maquina_de_estados(int Estado, float temperatura, int EstadoBoton1, int Est
     case P2:
       {
         u8g2.clearBuffer();
+        sprintf(sUmbral, "%d", Umbral);
         u8g2.setFont(u8g2_font_ncenB14_tr);
         u8g2.drawStr(15, 15, sUmbral);
         u8g2.sendBuffer();
@@ -70,10 +74,10 @@ void maquina_de_estados(int Estado, float temperatura, int EstadoBoton1, int Est
         if (temperatura >= Umbral) {
           digitalWrite(25, HIGH);
         }
-        if (digitalRead(34) == LOW) {
+        if (EstadoBoton1 == LOW) {
           Estado = SUMA;
         }
-        if (digitalRead(35) == LOW) {
+        if (EstadoBoton2 == LOW) {
           Estado = RESTA;
         }
         break;
